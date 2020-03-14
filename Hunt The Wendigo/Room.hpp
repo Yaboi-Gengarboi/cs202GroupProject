30bda6919481892018b67d88d211cecca19ec0ac
Justyn Durnford <jpdurnford@alaska.edu>
2020-03-13 20:13:34 -0700

    Added documentation for the Room class

--- Hunt The Wendigo/Room.hpp
+++ Hunt The Wendigo/Room.hpp
@@ -10,11 +10,14 @@
 #ifndef ROOM_HPP
 #define ROOM_HPP
 
-/*
-
+/* The Room class will function as nodes for the world grid
+   which the player, Wendigo and other objects will inhabit.
+   Each room has an ID for comparison reasons and shared_ptrs
+   to adjacent rooms. 
 */
 class Room
 {
+	// Identification method. -1 indicates an uninitialized room.
 	short int _id = -1;
 
 	/* These std::shared_ptrs will point to adjacent rooms.
@@ -25,23 +28,34 @@ class Room
 
 	public:
 
+	// Default constructor. Don't use.
 	Room();
 	
+	// Standard constructor.
 	Room(short int id);
 
+	// Destructor.
 	~Room();
 
+	// Returns _id.
 	short int ID() const;
 
+	// Sets the value of _id to id.
 	void setID(short int id);
 
+	// Returns a shared_ptr to the adjacent room specified by index.
 	std::shared_ptr<Room> getRoom(short int index) const;
 
+	/* Sets the value of the shared_ptr to the adjacent room specified
+	   by index to point to room.
+	*/
 	void setRoom(const Room& room, short int index);
 };
 
+// Returns true if the two rooms have the same ID.
 bool operator == (const Room& room1, const Room& room2);
 
+// Returns true if the two rooms are determined to be adjacent.
 bool areRoomsAdjacent(const Room& room1, const Room& room2);
 
 #endif // ROOM_HPP
